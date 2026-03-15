dcd6f3d000e9407b52dcc0af6c0af16c8f9abd8c
Ruslana <shyshkoruslana08@gmail.com>
2026-03-15 15:03:31 +0200

    add all in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,79 @@
 #include <iostream>
-
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include "Dish.h"
+#include "Order.h"
+#include "Customer.h"
+#include <utility>
+using namespace std;
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    cout << "===Customer1===" << endl;
+    Customer c1;
+    c1.printInfo();
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
 
-    return 0;// TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    cout << "-Dish-" << endl;
+    Dish d1;
+    d1.printInfo();
+
+
+    cout << "-Order-" << endl;
+    Order o1;
+    o1.printInfo();
+    cout << "-----------------------------" << endl;
+
+
+
+
+    cout << "===Customer2===" << endl;
+    Customer c2("Арінка");
+    c2.printInfo();
+
+
+    cout << "-Dish-" << endl;
+    Dish d2("Вареники");
+    d2.printInfo();
+
+
+    cout << "-Order-" << endl;
+    Order o2(34);
+    o2.printInfo();
+    cout << "-----------------------------" << endl;
+
+
+
+
+    cout << "===Customer3===" << endl;
+    Customer c3("Соня", 5);
+    c3.printInfo();
+
+
+    cout << "-Dish-" << endl;
+    Dish d3("Олівє", 67);
+    d3.printInfo();
+
+
+    cout << "-Order-" << endl;
+    Order o3(34, 6789);
+    o3.printInfo();
+    cout << "-----------------------------" << endl;
+
+
+
+    
+    cout << "===Customer4===" << endl;
+    Customer c4("Ання", 9, 423);
+    c4.printInfo();
+
+
+    cout << "-Dish-" << endl;
+    Dish d4("Борщ", 85.50, 300);
+    d4.printInfo();
+
+
+    cout << "-Order-" << endl;
+    Order o4(23, 405);
+    o4.printInfo();
+    cout << "-----------------------------" << endl;
+
+
+    return 0;
 }

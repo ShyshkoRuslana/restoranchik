9c80b472421fd09d3ca4b8030debe2a2dd3f5c65
Ruslana <shyshkoruslana08@gmail.com>
2026-03-19 09:19:30 +0200

    add copy constructor and operator + in class order

--- Order.cpp
+++ Order.cpp
@@ -1,21 +1,31 @@
 #include "Order.h"
 #include <iostream>
 using namespace std;
+
 Order::Order()
         :Order(0,0.0){}
-
 
 Order::Order(int ordernumber)
         :Order(ordernumber,0.0){}
 
-
 Order::Order(int ordernumber, double totalsum)
         :ordernumber(ordernumber),totalsum(totalsum) {}
 
+Order::Order(const Order& other) {
+       ordernumber=other.ordernumber;
+       totalsum=other.totalsum;
+
+}
 
 Order::~Order() {}
 
 
 void Order::printInfo()const{
     cout << "Order number: " << ordernumber << " | Total sum: " << totalsum << endl;
+}
+Order Order::operator+(const Order& other) {
+    return Order(
+            ordernumber + other.ordernumber,   // можна навіть просто залишити this->ordernumber
+            totalsum + other.totalsum
+    );
 }
--- Order.h
+++ Order.h
@@ -7,16 +7,19 @@ class Order {
 private:
     int ordernumber;
     double totalsum;
+    
 public:
     Order();
     Order(int ordernumber);
     Order(int ordernumber, double totalsum);
 
+    Order(const Order& other);
 
     ~Order();
 
-
+    Order operator+(const Order& other);
     void printInfo()const;
+
 };
 
 
